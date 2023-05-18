c78b0584e2c1d1c33c029603263769f4f82fb7bb
leileigwl <leileigwl11.12@gmail.com>
2023-05-18 14:48:32 +0800

    Auto Proxy初始化

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from core.ExportUtil import ExportCore
 from tel import config
+from tel.scrapy import Tel_Proxy
 
 if __name__ == '__main__':
-    export = ExportCore()
-    export.save_yml()
-    export.save_origin()  # 这个origin有了后就只能直接调用clash进行转化
-    print('xixix' + str(config.user_id))
+    # export = ExportCore()
+    # export.save_yml()
+    # export.save_origin()  # 这个origin有了后就只能直接调用clash进行转化
+    tel_proxy = Tel_Proxy()
+    tel_proxy.get()
--- tel/scrapy.py
+++ tel/scrapy.py
@@ -8,20 +8,25 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 api_id = config.api_id
 api_hash = config.api_hash
 user_id = config.user_id
-client = TelegramClient('anon', api_id, api_hash, proxy=("http", '127.0.0.1', 7890))
+client = TelegramClient('anon', api_id, api_hash)
 
 
-async def main():
-    await client.send_message(user_id, '/help')
+class Tel_Proxy():
+    def __init__(self):
+        pass
 
-    @client.on(events.NewMessage(from_users=user_id))
-    async def handler(event):
-        with open('session.txt', 'w', encoding='utf8') as f:
-            f.write(event.message.text)
-        await client.disconnect()
+    async def main(self):
+        await client.send_message(user_id, '/help')
 
-    await client.run_until_disconnected()
+        @client.on(events.NewMessage(from_users=user_id))
+        async def handler(event):
+            with open('session.txt', 'w', encoding='utf8') as f:
+                f.write(event.message.text)
+            await client.disconnect()
+
+        await client.run_until_disconnected()
+
+    async def get(self):
+        client.loop.run_until_complete(self.main())
 
 
-with client:
-    client.loop.run_until_complete(main())
