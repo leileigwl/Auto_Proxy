f9141f8c744bf06ff859087e63a52f5ef62113f3
leileigwl <leileigwl11.12@gmail.com>
2023-05-18 14:43:18 +0800

    Auto Proxy初始化

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     export = ExportCore()
     export.save_yml()
     export.save_origin()  # 这个origin有了后就只能直接调用clash进行转化
-    print('xixix' + config.api_id)
+    print('xixix' + str(config.user_id))
