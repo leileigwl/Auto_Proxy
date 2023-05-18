c34f0143365de468cb75246eed5696b5c1972d52
leileigwl <leileigwl11.12@gmail.com>
2023-05-18 15:35:45 +0800

    auto tel

--- tel/scrapy.py
+++ tel/scrapy.py
@@ -5,26 +5,32 @@ import logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-api_id = config.api_id
-api_hash = config.api_hash
-user_id = config.user_id
-client = TelegramClient('tel/anon.session', api_id, api_hash)
-
 
 class Tel_Proxy():
     def __init__(self):
-        pass
+        self.api_id = config.api_id
+        self.api_hash = config.api_hash
+        self.user_id = config.user_id
+        self.client = TelegramClient('anon', self.api_id, self.api_hash)
+        self.client.connect_timeout = 3.0
+        self.client.connect_retries = 2
 
     async def main(self):
-        await client.send_message(user_id, '/help')
+        await self.client.send_message(self.user_id, '/help')
 
-        @client.on(events.NewMessage(from_users=user_id))
+        @self.client.on(events.NewMessage(from_users=self.user_id))
         async def handler(event):
             with open('session.txt', 'w', encoding='utf8') as f:
                 f.write(event.message.text)
-            await client.disconnect()
+            await self.client.disconnect()
 
-        await client.run_until_disconnected()
+        await self.client.run_until_disconnected()
 
-    async def get(self):
-        client.loop.run_until_complete(self.main())
+    def get(self):
+        with self.client:
+            self.client.loop.run_until_complete(self.main())
+
+
+if __name__ == '__main__':
+    tel_p = Tel_Proxy()
+    tel_p.get()
